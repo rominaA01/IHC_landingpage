779b9bb6db53b080f580b3c5f31eb5b14693a67a
Frida Dam <147429260+Frida-Dam@users.noreply.github.com>
2023-11-22 02:36:17 -0500

    Update scriptLP.js

--- scriptLP.js
+++ scriptLP.js
@@ -1,327 +1,61 @@
 
-h1{
-    
-    font-family:Verdana, Geneva, Tahoma, sans-serif;
-    font-weight: 500;
+function Res1(){
+    document.querySelector(".resenia3").style.display = "none";
+    document.querySelector(".resenia4").style.display = "none";
+
+    document.querySelector('.resenia1').style.display = "block";
+    document.querySelector('.resenia2').style.display = "block";
 }
 
-h2{
-    color: #000;
-    font-family:Verdana, Geneva, Tahoma, sans-serif;
-    font-weight: 100;
-    
+function Res2(){
+    document.querySelector(".resenia1").style.display = "none";
+    document.querySelector(".resenia2").style.display = "none";
+
+    document.querySelector('.resenia3').style.display = "block";
+    document.querySelector('.resenia4').style.display = "block";
 }
 
-@media screen and (max-width:700px){
-    h1{
-        padding-left: 30px;
-        font-size: 38px;
+document.addEventListener("DOMContentLoaded", function() {
+    function abrirFormulario() {
+        var formulario = document.querySelector(".formulario");
+        var fondoOscuro = document.createElement("div");
+        fondoOscuro.className = "fondo-oscuro";
+        document.body.appendChild(fondoOscuro);
+        formulario.classList.add("abierto");
+
+        fondoOscuro.addEventListener("click", function() {
+            formulario.classList.remove("abierto");
+            fondoOscuro.style.display = "none";
+            document.body.removeChild(fondoOscuro);
+        });
     }
-    h2{
-        padding-left: 30px;
-        padding-right: 90px;
-        font-size: 15px;
+
+    function cerrarFormulario() {
+        var formulario = document.querySelector(".formulario");
+        var fondoOscuro = document.querySelector(".fondo-oscuro");
+        formulario.classList.remove("abierto");
+        fondoOscuro.style.display = "none";
+        document.body.removeChild(fondoOscuro); 
     }
-} 
 
-.Barra {
-    color: black;
-    width: 100% - 100px ;
-    height: 80px;
-    background-color:rgba(191, 191, 191, 0.751);
-    font-size: 18px;
-    font-weight: 500;
-    text-align: center;
+    document.querySelector(".BtnRegistro").addEventListener("click", abrirFormulario);
+    document.querySelector(".cerrar").addEventListener("click", cerrarFormulario);
     
+});
+
+function toggleChatbox() {
+    var chatbox = document.getElementById("chatbox");
+    chatbox.style.display = (chatbox.style.display === "none") ? "block" : "none";
 }
 
-.B1, .B2, .B3 {
-    float: left; /* Coloca los elementos en línea uno al lado del otro */
-    width: calc(33.33% ); /* Calcula el ancho del 33.33% (el -20px compensa el margen) */
-}
+function enviarMensaje() {
+    // Obtener el mensaje del input
+    var mensaje = document.getElementById("mensajeInput").value;
 
-.SecInicio {
-    width: auto;
-    height: 600px;
-    background-color:white;
-    padding-left: 90px;
-    display: flex;
-    
-}
+    // Mostrar un mensaje de confirmación
+    alert("Mensaje enviado: " + mensaje);
 
-.textoInicio{
-    font-weight: 500;
-    padding-top: 90px;
-    flex: 1; /* Hace que ocupe todo el espacio disponible en la mitad izquierda */
-    text-align: left; /* Alinea el texto a la izquierda dentro del espacio disponible */
-    flex: column;
+    // Limpiar el input después de enviar el mensaje
+    document.getElementById("mensajeInput").value = "";
 }
-.SecInicio h2{
-    font-size: 25px;
-    padding-top: 80px;
-    padding-bottom: 90px;
-}
-
-.SecInicio h1{
-    font-size: 35px;
-    font-weight: bold;
-}
-.ImagenInicio{
-    width: 65%;
-    height: 550px;
-    flex: 1; /* Hace que ocupe todo el espacio disponible en la mitad derecha */
-    padding: 20px; /* Puedes ajustar este valor según tus necesidades */
-    text-align: right;
-}
-
-.ImagenInicio img{
-    max-width: 100%;
-    height: 550px;
-}
-
-.BtnRegistro{
-    width: 120px;
-    height: 40px;
-    border-color: #000;
-    border: 2px solid;
-    font-size: 20px;
-    padding-left: 10px;
-    text-align: center;
-    cursor:pointer;
-    
-}
- .formulario{
-    display: none;
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 300px;
-    background-color: white;
-    padding: 20px;
-    border: 1px solid #ccc;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    text-align: center;
-    transition: all 0.3s ease;
- }
-
-.SecAbtUs {
-    color: white;
-    width: 100% - 90px;
-    height: 200px;
-    background-color: rgba(41, 94, 252, 0.804);
-    font-size: 30px;
-    font-weight: 500;
-    padding-bottom: 40px;
-    padding-top: 100px ;
-    padding-left: 90px;
-    padding-right: 50px;
-    
-}
-
-.SecAbtUs td:first-child {
-    font-size: 50px;
-    font-weight: bold;
-    padding-right: 50px;
-}
-
-.VideoAbtUs {
-    color: white;
-    width: 100% - 90px;
-    height: 600px;
-    background-color: rgba(41, 94, 252, 0.804);
-    font-size: 30px;
-    font-weight: 500;
-    padding-bottom: 40px;
-    padding-top: 30px ;
-    text-align: center;
-    
-    
-}
-
-.Beneficios {
-    color: black;
-    width: 100% - 100px ;
-    height: 500px;
-    background-color:white;
-    font-size: 20px;
-    font-weight: 500;
-    padding-bottom: 72px;
-    padding-top: 72px ;
-    padding-left: 100px;
-    padding-right: 100px;
-    text-align: center;
-    
-}
-
-.Bn1, .Bn2, .Bn3 {
-    margin: 10px; /* Espacio entre elementos */
-    padding-top: 80px;
-    padding-bottom: 80px;
-    float: left; /* Coloca los elementos en línea uno al lado del otro */
-    width: calc(33.33% - 20px); /* Calcula el ancho del 33.33% (el -20px compensa el margen) */
-}
-
-.resenia{
-    width: 100% ;
-    height: 30px;
-    padding-top: 10px;
-    background-color: rgba(120, 248, 248, 0.797);
-    text-align: center;
-}
-
-.carousel{
-    color: black;
-    width:auto ;
-    height: 400px;
-    background-color: rgba(120, 248, 248, 0.797);
-    font-size: 20px;
-    font-weight: 500;
-    padding-bottom: 72px;
-    padding-top: 72px ;
-    padding-left: 50px;
-    padding-right: 50px;
-    text-align: center;
-    display: flex;
-    overflow: hidden;
-}
-
-
-
-.resenia1, .resenia2{
-    width: calc(50% - 90px);
-    height: 400px;
-    margin: 35px;
-    margin-top: 0px;
-    background-color: aliceblue;
-    text-align: center;
-    
-}
-
-.resenia3, .resenia4{
-    display: none;
-    width: calc(50% - 90px);
-    height: 400px;
-    margin: 35px;
-    margin-top: 0px;
-    background-color: aliceblue;
-    text-align: center;
-}
-
-.carousel i{
-    
-    padding-left: 10px;
-    
-}
-
-.nombre{
-    padding-top: 40px;
-    text-align: left;
-    font-size: 28px;
-}
-.comentario{
-    padding-top: 70px;
-    padding-left: 40px;
-    padding-right: 40px;
-    text-align: left;
-    font-size: 18px;
-}
-
-.resenia1 img, .resenia2 img, .resenia3 img, .resenia4 img{
-    width: 135px;
-    height: 150px;
-    float: left;
-    padding: 40px;
-    padding-bottom: 20px;
-}
-
-.app {
-    color: white;
-    width: 100% - 100px ;
-    height: 500px;
-    background-color:rgba(41, 94, 252, 0.804);
-    font-size: 20px;
-    font-weight: 500;
-    padding-bottom: 72px;
-    padding-top: 72px ;
-    padding-left: 100px;
-    padding-right: 100px;
-    text-align: center;
-    
-}
-
-.app1, .app2 {
-    margin: 10px; /* Espacio entre elementos */
-    padding-top: 20px;
-    font-size: 25px;
-    padding-bottom: 80px;
-    float: left; /* Coloca los elementos en línea uno al lado del otro */
-    width: calc(50% - 20px); /* Calcula el ancho del 33.33% (el -20px compensa el margen) */
-}
-
-.Soporte{
-    width: auto;
-    height: 170px;
-    background-color: white;
-    padding-top: 75px;
-    padding-left: 155px;
-   
-}
-.chatbox {
-    display: none;
-    border: 1px solid #ccc;
-    padding: 10px;
-    margin: 20px;
-    max-width: 400px;
-    overflow: auto;
-}
-.Final {
-    color: black;
-    width: 100% - 100px ;
-    height: 400px;
-    background-color:rgb(161, 161, 161);
-    font-size: 20px;
-    font-weight: 500;
-    padding-left: 100px;
-    padding-right: 100px;
-    text-align: center;
-    
-}
-.F1, .F2, .F3 {
-    margin: 10px; /* Espacio entre elementos */
-    padding-top: 30px;
-    padding-bottom: 80px;
-    float: left; /* Coloca los elementos en línea uno al lado del otro */
-    width: calc(33.33% - 20px); /* Calcula el ancho del 33.33% (el -20px compensa el margen) */
-}
-.Soporte td:first-child {
-    padding-right: 480px;
-}
-
-.botonSoporte{
-    font-size: 22px;
-}
-   
-  
-  /* Estilo del botón para cerrar el formulario */
-  .cerrar {
-    margin-top: 10px;
-  }
-  
-  /* Estilo de fondo oscuro al abrir el formulario */
-  .fondo-oscuro {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(187, 47, 47, 0.5);
-    display: none;
-    z-index: 1;
-  }
-  
-  /* Estilo de animación al abrir el formulario */
-  .abierto {
-    display: block;
-  }
 
