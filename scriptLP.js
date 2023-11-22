3bbed9a530b1804b2d1608a023ae0ed80060d967
Frida Dam <147429260+Frida-Dam@users.noreply.github.com>
2023-11-21 19:58:44 -0500

    Update scriptLP.js

--- scriptLP.js
+++ scriptLP.js
@@ -14,3 +14,31 @@ function Res2(){
     document.querySelector('.resenia3').style.display = "block";
     document.querySelector('.resenia4').style.display = "block";
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    function abrirFormulario() {
+        var formulario = document.querySelector(".formulario");
+        var fondoOscuro = document.createElement("div");
+        fondoOscuro.className = "fondo-oscuro";
+        document.body.appendChild(fondoOscuro);
+        formulario.classList.add("abierto");
+
+        fondoOscuro.addEventListener("click", function() {
+            formulario.classList.remove("abierto");
+            fondoOscuro.style.display = "none";
+            document.body.removeChild(fondoOscuro);
+        });
+    }
+
+    function cerrarFormulario() {
+        var formulario = document.querySelector(".formulario");
+        var fondoOscuro = document.querySelector(".fondo-oscuro");
+        formulario.classList.remove("abierto");
+        fondoOscuro.style.display = "none";
+        document.body.removeChild(fondoOscuro); 
+    }
+
+    document.querySelector(".BtnRegistro").addEventListener("click", abrirFormulario);
+    document.querySelector(".cerrar").addEventListener("click", cerrarFormulario);
+});
+
